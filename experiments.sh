9197de23847a402e199ce2e7dfa2974a87e6bbbd
Jacqueline <35910144+HaoyunHong@users.noreply.github.com>
2021-07-05 19:19:33 +0800

    Update experiments.sh

--- experiments.sh
+++ experiments.sh
@@ -8,11 +8,11 @@ python run_LaBSE_neighbor.py
 echo "Train SeflKG with LaBSE embedding and neighbor information on DWY100K"
 python run_DWY_LaBSE_neighbor.py
 
-echo "Train SeflKG with only LaBSE embedding on DWY100K"
-python run_DWY_LaBSE_SSL.py
+echo "Train SeflKG with only LaBSE embedding on DBP15K"
+python run_LaBSE_SSL.py
 
 echo "Train SeflKG with only LaBSE embedding on DWY100K"
-python run_DWY_LaBSE_SSL_DWY.py
+python run_LaBSE_SSL_DWY.py
 
 echo "Train SeflKG with LaBSE embedding and neighbor information on DBP15K adding noise"
 python run_LaBSE_neighbor_noise.py
