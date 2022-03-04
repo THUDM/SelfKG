2ec133a6add999f86ddcf9b183a82e0b0ec88b17
Shaw <xiao190525@gmail.com>
2022-03-04 10:43:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,24 +6,13 @@ echo "create conda environment 'selfkg'"
 conda create -n selfkg
 
 echo "activate selfkg"
-conda activate selfkg
+source activate selfkg
 
 echo "install pytorch"
-conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch # change according to your need here
+conda install -n selfkg pytorch=1.9.0 torchvision torchaudio cudatoolkit=10.2 -c pytorch # change according to your need here
 
-echo "install faiss-cpu"
-conda install faiss-cpu=1.7.1 -c pytorch
+echo "install faiss-cpu=1.7.1"
+conda install -n selfkg faiss-cpu=1.7.1 -c pytorch
 
-echo "install numpy"
-conda install numpy=1.21
-
-echo "install pandas"
-conda install pandas=1.2.5
-
-echo "install tqdm"
-conda install tqdm=4.61.1
-
-echo "install transformers"
-conda install transformers=4.8.2
-
-echo "Environment setup"
+echo "install numpy=1.21 pandas=1.2.5 tqdm=4.61.1 transformers=4.8.2"
+pip install numpy==1.21 pandas==1.2.5 tqdm==4.61.1 transformers==4.8.2
