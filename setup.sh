c489822cb77257e23d454ba18b94fd510a17196d
Shaw <xiao190525@gmail.com>
2022-03-04 10:12:08 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,18 +12,18 @@ echo "install pytorch"
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch # change according to your need here
 
 echo "install faiss-cpu"
-conda install faiss-cpu -c pytorch
+conda install faiss-cpu=1.7.1 -c pytorch
 
 echo "install numpy"
-conda install numpy
+conda install numpy=1.21
 
 echo "install pandas"
-conda install pandas
+conda install pandas=1.2.5
 
 echo "install tqdm"
-conda install tqdm
+conda install tqdm=4.61.1
 
 echo "install transformers"
-conda install transformers
+conda install transformers=4.8.2
 
 echo "Environment setup"
