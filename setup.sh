cda0b8a88022c5bde9f3f1132b4fa4dda74ce8e7
洪昊昀 <2017010591@secoder.net>
2022-03-08 00:40:11 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,5 +14,5 @@ conda install -n selfkg pytorch=1.9.0 torchvision torchaudio cudatoolkit=10.2 -c
 echo "install faiss-cpu=1.7.1"
 conda install -n selfkg faiss-cpu=1.7.1 -c pytorch
 
-echo "install numpy=1.21 pandas=1.2.5 tqdm=4.61.1 transformers=4.8.2"
-pip install numpy==1.21 pandas==1.2.5 tqdm==4.61.1 transformers==4.8.2 torchtext==0.10.0
+echo "install numpy=1.19.2 pandas=1.0.5 tqdm=4.61.1 transformers=4.8.2"
+pip install numpy==1.19.2 pandas==1.0.5 tqdm==4.61.1 transformers==4.8.2 torchtext==0.10.0
