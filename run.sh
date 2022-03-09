52f71c186ab4ad2db8de6cadf4e498d6e563ee96
Jacqueline <35910144+HaoyunHong@users.noreply.github.com>
2022-03-09 10:51:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 echo "Start Training"
 
 echo "Train SeflKG with LaBSE embedding and neighbor information on DBP15K"
-python run_LaBSE_neighbor.py
+python run_LaBSE_neighbor.py --device "cuda:0"  --epoch 150 # you can change the command line parameters as you like
 
 echo "Train SeflKG with LaBSE embedding and neighbor information on DWY100K"
-python run_DWY_LaBSE_neighbor.py
+python run_DWY_LaBSE_neighbor.py --device "cuda:0" --epoch 150 # you can change the command line parameters as you like
